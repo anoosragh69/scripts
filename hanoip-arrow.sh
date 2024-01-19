da17d9270aa33029ee82cdd849e917483fd29bd2
Anoosragh <ok.ano0s6090@gmail.com>
2024-01-19 05:06:16 +0000

    hanoip: Intial script
    Signed-off-by: Anoosragh <ok.ano0s6090@gmail.com>

---
+++ hanoip-arrow.sh
@@ -0,0 +1,25 @@
+# Build Script for hanoip
+
+repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 --git-lfs -g default,-mips,-darwin,-notdefault
+
+# Remove local_manifests directory
+rm -rf .repo/local_manifests
+
+# Clone the manifest repository
+git clone https://github.com/anoosragh69/local_manifests-moto -b 13 --depth 1 .repo/local_manifests
+cd .repo/loca* && rm aosp.mk lineage.mk bliss.mk
+
+# Force sync the repository
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# Set up the build environment
+source build/envsetup.sh
+
+# Apply sepolicy patch
+sudo ./device/motorola/targets/scripts/replace_camera_sepolicy.sh
+
+# Lunch for CPH1859 device
+lunch arrow_hanoip-userdebug
+
+# Build for CPH1859 device
+mka bacon
