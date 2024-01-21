bcde8c434304da548d3065dfd404b5df925d10fa
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-01-21 17:46:04 +0530

    Update derpfest-rhodep.sh

--- derpfest-rhodep.sh
+++ derpfest-rhodep.sh
@@ -16,6 +16,8 @@ sudo apt install ccache -y
 # Set up the build environment
 source build/envsetup.sh
 
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+
 # Lunch for CPH1859 device
 lunch derp_rhodep-userdebug
 
