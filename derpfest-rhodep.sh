2608ce4eaf983352e7585742270a1b805d1d01ef
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-01-21 17:12:14 +0530

    Create derpfest-rhodep.sh

---
+++ derpfest-rhodep.sh
@@ -0,0 +1,19 @@
+# Build Script for hanoip
+
+# Remove local_manifests directory
+rm -rf .repo/local_manifests
+
+# Clone the manifest repository
+git clone https://github.com/anoosragh69/local_manifests-moto -b 13-rhodep-derpfest --depth 1 .repo/local_manifests
+
+# Force sync the repository
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+# Set up the build environment
+source build/envsetup.sh
+
+# Lunch for CPH1859 device
+lunch derp_rhodep-userdebug
+
+# Build for CPH1859 device
+m updatepackage
