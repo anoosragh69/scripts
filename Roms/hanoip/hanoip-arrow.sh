f490580aff66c90cfacae9271e221e5bd059f81d
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-02-05 00:49:59 +0530

    Update and clean up script

--- Roms/hanoip/hanoip-arrow.sh
+++ Roms/hanoip/hanoip-arrow.sh
@@ -1,31 +1,33 @@
 # Build Script for hanoip
 
+## Nuke old source
 rm -rf .repo
 rm -rf *
 
+## Sync arrow
 repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-13.1 --git-lfs -g default,-mips,-darwin,-notdefault
 
-# Remove local_manifests directory
+## Remove local_manifests directory
 rm -rf .repo/local_manifests
 
-# Clone the manifest repository
+## Clone the manifest repository
 git clone https://github.com/anoosragh69/local_manifests-moto -b 13-arrow --depth 1 .repo/local_manifests
-# Force sync the repository
+
+## Force sync the repository
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
+## Install ccache
 sudo apt update
 sudo apt install ccache -y
 
-# Set up the build environment
+## Set up the build environment
 source build/envsetup.sh
 
-# Apply sepolicy patch
+## Apply sepolicy patch
 sudo ./device/motorola/targets/scripts/replace_camera_sepolicy.sh
 
-# Lunch for CPH1859 device
+## Lunch for hanoip device
 lunch arrow_hanoip-userdebug
 
-# Build for CPH1859 device
+## Build for hanoip device
 mka bacon
