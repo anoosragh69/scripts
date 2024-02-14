4f7d826b4be0062b90891a9c33f2e4042913f9e0
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-02-14 09:57:48 +0530

    Temp

--- Roms/hanoip/hanoip-derp.sh
+++ Roms/hanoip/hanoip-derp.sh
@@ -1,10 +1,14 @@
 # Build Script for hanoip
 
+## Temp
+rm -rf .repo
+rm -rf *
+
 ## Remove local_manifests directory
-rm -rf .repo/local_manifests
+# rm -rf .repo/local_manifests
 
 ## Remove unstaged repos
-rm -rf prebuilts/clang/host/linux-x86/ prebuilts/rust/ 
+# rm -rf prebuilts/clang/host/linux-x86/ prebuilts/rust/ 
 
 ## Clone the manifest repository
 repo init --depth=1 -u https://github.com/DerpFest-AOSP/manifest.git -b 14
