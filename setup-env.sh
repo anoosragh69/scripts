e6ec8158be8c3839b3df5e1449fc67b014a43266
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-03-01 14:38:55 +0530

    Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,26 +1,18 @@
-# get superuser access.
-
-sudo su
-
 # install JDK (press enter 2 times).
 
-add-apt-repository ppa:openjdk-r/ppa -y
+sudo add-apt-repository ppa:openjdk-r/ppa -y
 
 # update all packages.
 
-apt-get update
+sudo apt-get update
 
 # install more packages.
 
-apt install openssh-server screen python git openjdk-8-jdk android-tools-adb bc bison \
+sudo apt install openssh-server screen python git openjdk-8-jdk android-tools-adb bc bison \
 build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev \
 lib32readline-dev lib32z1-dev  liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev \
 libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev \
 libtinfo5 libncurses5 -y
-
-# become a normal user.
-
-exit
 
 # creating a bin folder.
 
