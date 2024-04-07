bb00bdb742262da185a1e00a1530818f89fa6e94
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-04-08 00:19:05 +0530

    Add device-tree-compiler to package list

--- setup-env.sh
+++ setup-env.sh
@@ -9,8 +9,8 @@ sudo apt-get update
 # install more packages.
 
 sudo apt install openssh-server screen python3 git default-jdk android-tools-adb bc bison \
-build-essential curl flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev \
-lib32readline-dev lib32z1-dev  liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev \
+build-essential curl device-tree-compiler flex g++-multilib gcc-multilib gnupg gperf imagemagick lib32ncurses-dev \
+lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev \
 libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc yasm zip zlib1g-dev \
 libtinfo5 libncurses5 -y
 
