c53b3145cb003cd89adc67b0a840e089f954d96e
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-04-09 00:19:41 +0530

    hanoip: Initial hanoip kernel upload script

---
+++ kernel-upload.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
+# Copyright (C) 2018 Rama Bndan Prakoso (rama982)
+
+date=$(date +"%Y%m%d-%H%M%S")
+ver=v1.0
+
+# Zip Image
+gzip out/target/product/hanoip/obj/KERNEL_OBJ/arch/arm64/boot/Image
+
+# Move files to anykernel
+cp -f out/target/product/hanoip/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz AnyKernel3/Image.gz
+cp -f out/target/product/hanoip/obj/KERNEL_OBJ/arch/arm64/boot/dts/qcom/sdmmagpie-hanoi-base.dtb AnyKernel3/dtb
+cp -f out/target/product/hanoip/dtbo.img AnyKernel3/dtbo.img
+
+# Zip kernel
+cd Anykernel3
+zip -r9 shinzui-hanoip.zip * -x .git README.md
+
+# Sign zip file
+curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
+java -jar zipsigner-3.0.jar shinzui-hanoip.zip shinzui-$version-$date.zip
+
+# Upload kernel
+ksau upload shinzui-v* Public
