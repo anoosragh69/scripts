ca0f3bac6dbb61612c6ba053bad083328772519c
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-04-09 14:22:04 +0530

    Initial yaap script

---
+++ Roms/hanoip/hanoip-yaap.sh
@@ -0,0 +1,28 @@
+# Build Script for hanoip
+
+## Remove old repos
+rm -rf prebuilts/clang/ prebuilts/rust/ .repo/local_manifests
+
+## Clone the manifest repository
+repo init --depth=1 -u https://github.com/yaap/manifest.git -b fourteen --git-lfs
+
+git clone https://gitlab.com/anoosragh69/local_manifests.git -b yaap-hanoip --depth 1 .repo/local_manifests
+
+## Force sync the repository
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+## Install ccache
+sudo apt update
+sudo apt install ccache -y
+
+## Set up the build environment
+source build/envsetup.sh
+
+export BUILD_HOSTNAME=mtrixfuker
+export BUILD_USERNAME=anoosragh69
+
+## Lunch for hanoip device
+lunch yaap_hanoip-userdebug
+
+## Build for hanoip device
+m yaap
