7305e12f1b3e4f5a86a5bf2d343833c1d7b837f1
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-04-10 10:28:41 +0530

    hanoip: lmodroid: Intial script

---
+++ Roms/hanoip/hanoip-lmo.sh
@@ -0,0 +1,28 @@
+# Build Script for hanoip
+
+## Remove old repos
+rm -rf .repo/local_manifests packages hardware lineage-sdk external frameworks prebuilts/gcc
+
+## Clone the manifest repository
+repo init --depth=1 --no-repo-verify -u https://git.libremobileos.com/LMODroid/manifest.git -b fourteen --git-lfs -g default,-mips,-darwin,-notdefault
+
+git clone https://gitlab.com/anoosragh69/local_manifests.git -b lmo-hanoip --depth 1 .repo/local_manifests
+
+## Force sync the repository
+/opt/crave/resync.sh
+
+## Install ccache
+sudo apt update
+sudo apt install ccache -y
+
+## Set up the build environment
+source build/envsetup.sh
+
+export BUILD_HOSTNAME=mtrixfuker
+export BUILD_USERNAME=anoosragh69
+
+## Lunch for hanoip device
+lunch lmodroid_hanoip-ap1a-userdebug
+
+## Build for hanoip device
+m bacon
